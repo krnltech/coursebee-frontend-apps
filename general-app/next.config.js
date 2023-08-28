b320e2b843aca1d5accb4cdbfda223c0a09f4570
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2023-08-28 21:55:08 +0600

    next build directory change

--- general-app/next.config.js
+++ general-app/next.config.js
@@ -3,6 +3,7 @@ const {i18n} = require("./next-i18next.config.js");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: './general-app/build',
   i18n,
 }
 
